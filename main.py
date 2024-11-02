54f355a7b3f49922e27f984d86ceabe976f988a2
11praveen11 <praveenmax1198@gmail.com>
2024-11-02 15:02:46 +0530

    hide sensitive keys in env file

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ if abs(diff_percent) > 0.07:
 
 #Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
     three_articles = articles
-    #print(three_articles)
+    print(three_articles)
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
@@ -80,18 +80,8 @@ if abs(diff_percent) > 0.07:
     for article in formatted_article_list:
         message = client.messages.create(
             body=article,
-            from_="+12094384790",
-            to="+919740708889"
+            from_=os.environ.get('TWILIO_PHONE_NUMBER'),
+            to=os.environ.get('USER_PHONE_NUMBER')
         )
 
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
